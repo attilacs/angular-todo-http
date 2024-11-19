42e417011b304ea86055ba488334263c4b541a62
attilacs <csanyiattila23@gmail.com>
2024-11-19 18:09:39 +0100

    Implement edit title function

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,17 +1,29 @@
-import { Component, inject, input } from "@angular/core";
+import {
+	Component,
+	type ElementRef,
+	inject,
+	input,
+	signal,
+	ViewChild,
+} from "@angular/core";
 import type { Todo } from "../../interfaces/todo";
 import { TodoService } from "../../services/todo.service";
+import { FormsModule } from "@angular/forms";
 
 @Component({
 	selector: "app-todo-item",
 	standalone: true,
-	imports: [],
+	imports: [FormsModule],
 	templateUrl: "./todo-item.component.html",
 	styleUrl: "./todo-item.component.scss",
 })
 export class TodoItemComponent {
 	private todoService = inject(TodoService);
 	todo = input.required<Todo>();
+	isEditing = signal(false);
+	title = "";
+
+	@ViewChild("todoInputRef") inputRef?: ElementRef;
 
 	toggleTodo() {
 		const todo: Todo = {
@@ -21,7 +33,34 @@ export class TodoItemComponent {
 		this.todoService.updateTodo(todo);
 	}
 
+	updateTitle() {
+		const todo: Todo = {
+			...this.todo(),
+			title: this.title,
+		};
+		this.todoService.updateTodo(todo);
+		this.isEditing.set(false);
+	}
+
+	startEdit() {
+		this.isEditing.set(true);
+	}
+
 	deleteTodo() {
 		this.todoService.deleteTodo(this.todo().id);
+	}
+
+	handleFocus() {
+		this.title = this.todo().title;
+	}
+
+	handleBlur() {
+		this.isEditing.set(false);
+	}
+
+	ngAfterViewChecked(): void {
+		if (this.isEditing()) {
+			this.inputRef?.nativeElement.focus();
+		}
 	}
 }
