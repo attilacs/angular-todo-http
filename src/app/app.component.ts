177da6bc99a6d17e43dc7e0691ab582adb43f845
attilacs <csanyiattila23@gmail.com>
2024-11-20 11:18:26 +0100

    Create todo-footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from "@angular/core";
 import { TodoListComponent } from "./components/todo-list/todo-list.component";
 import { TodoHeaderComponent } from "./components/todo-header/todo-header.component";
+import { TodoFooterComponent } from "./components/todo-footer/todo-footer.component";
 
 @Component({
 	selector: "app-root",
 	standalone: true,
-	imports: [TodoHeaderComponent, TodoListComponent],
+	imports: [TodoHeaderComponent, TodoListComponent, TodoFooterComponent],
 	templateUrl: "./app.component.html",
 	styleUrl: "./app.component.scss",
 })
