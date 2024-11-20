10c8059313bea1962c095197ce90bdb90531505d
attilacs <csanyiattila23@gmail.com>
2024-11-20 13:47:46 +0100

    Add routes

--- src/app/components/todo-footer/todo-footer.component.ts
+++ src/app/components/todo-footer/todo-footer.component.ts
@@ -1,15 +1,18 @@
 import { Component, computed, inject } from "@angular/core";
 import { TodoService } from "../../services/todo.service";
+import { RouterLink } from "@angular/router";
+import { Location } from "@angular/common";
 
 @Component({
 	selector: "app-todo-footer",
 	standalone: true,
-	imports: [],
+	imports: [RouterLink],
 	templateUrl: "./todo-footer.component.html",
 	styleUrl: "./todo-footer.component.scss",
 })
 export class TodoFooterComponent {
 	private todoService = inject(TodoService);
+	private location = inject(Location);
 	activeTodosCount = this.todoService.activeTodosCount;
 	allTodosCount = this.todoService.allTodosCount;
 	someTodoComleted = this.todoService.someTodoComleted;
@@ -20,5 +23,9 @@ export class TodoFooterComponent {
 
 	clearCompleted() {
 		this.todoService.clearCompleted();
+	}
+
+	get path(): string {
+		return this.location.path().split("/")[1] || "all";
 	}
 }
